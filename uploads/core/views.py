8b3ab4312fd9482fd9cc2e0e99159fac8488a323
Nazarbek <nazbekktl@gmail.com>
2017-12-26 01:49:37 +0600

    Restored everything

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -25,5 +25,17 @@ def simple_upload(request):
         return render(request, 'core/simple_upload.html', {
             'uploaded_file_url': uploaded_file_url, "result": lines
         }) 
-    return render(request, 'core/home.html')
+
+        return render(request, 'core/simple_upload.html')
  
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
