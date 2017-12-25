f2c4f46842bad97d0d2a1008af3afeed555a9219
Nazarbek <nazbekktl@gmail.com>
2017-12-25 22:34:17 +0600

    Project creation

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -4,7 +4,10 @@ from django.core.files.storage import FileSystemStorage
 
 from uploads.core.models import Document
 from uploads.core.forms import DocumentForm
-
+from uploads.core.classify_image import run_inference_on_image, maybe_download_and_extract
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+from subprocess import call
 
 def home(request):
     documents = Document.objects.all()
@@ -17,9 +20,11 @@ def simple_upload(request):
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+        return_code = call("python uploads/core/classify_image.py --image_file "+settings.MEDIA_ROOT+"/"+filename+" >123.out", shell=True)  
+        lines = [line.rstrip('\n') for line in open('123.out')]
         return render(request, 'core/simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+            'uploaded_file_url': uploaded_file_url, "result": lines
+        }) 
     return render(request, 'core/simple_upload.html')
 
 
@@ -34,3 +39,4 @@ def model_form_upload(request):
     return render(request, 'core/model_form_upload.html', {
         'form': form
     })
+ 
